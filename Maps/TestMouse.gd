b3e35f84eb281f1351cb5ff9bf2ee007e3b2afe0
kavehghalambor <kghalamb@calpoly.edu>
2024-03-10 19:14:32 -0700

    changed map position

--- Maps/TestMouse.gd
+++ Maps/TestMouse.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+var mouse_position = null
+var is_mouse_down := false
+@onready var ball = $Ball
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,9 +13,22 @@ func _ready():
 func _process(delta):
 	pass
 
-var mouse_position = null
-var is_mouse_down := null
- @onready var ball = $Ball
+
 
 func _input(event):
-	
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.is_pressed():
+				is_mouse_down = true
+			else:
+				is_mouse_down = false
+	if event is InputEventMouse and is_mouse_down:
+		mouse_position = get_local_mouse_position()
+	else:
+		mouse_position = null
+	queue_redraw()
+
+func _draw():
+	if mouse_position != null:
+		draw_line(ball.position, mouse_position, Color.YELLOW, 6)
+				
